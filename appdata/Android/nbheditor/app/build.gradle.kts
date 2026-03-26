621a592fa81e13e1a3da23472bee53d2b974e401
Beeta-inc <beeta.main@gmail.com>
2026-03-26 15:20:37 +0530

    final updates for version1

--- appdata/Android/nbheditor/app/build.gradle.kts
+++ appdata/Android/nbheditor/app/build.gradle.kts
@@ -16,9 +16,19 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file("../beeta-release.jks")
+            storePassword = "beeta2024"
+            keyAlias = "nbheditor"
+            keyPassword = "beeta2024"
+        }
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("release")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
