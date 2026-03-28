25122cdf3097101cb6c8f8e088f5cfd35a82fac1
Beeta-inc <beeta.main@gmail.com>
2026-03-28 15:12:01 +0530

    bug fixes

--- appdata/Android/nbheditor/app/build.gradle.kts
+++ appdata/Android/nbheditor/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.beeta.nbheditor"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "2.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
