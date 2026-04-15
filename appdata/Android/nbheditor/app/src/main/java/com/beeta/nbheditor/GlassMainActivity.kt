b470a42841990188e82da6cb5cf7645006c13e05
beeta-inc <beeta.main@gmail.com>
2026-04-15 17:06:20 +0530

    bug fixes

--- appdata/Android/nbheditor/app/src/main/java/com/beeta/nbheditor/GlassMainActivity.kt
+++ appdata/Android/nbheditor/app/src/main/java/com/beeta/nbheditor/GlassMainActivity.kt
@@ -2,7 +2,6 @@ package com.beeta.nbheditor
 
 /**
  * Identical to MainActivity but launched with Theme.Nbheditor.Glass
- * declared in the manifest — this is the only reliable way to set
- * windowIsTranslucent=true before the window is created.
+ * ment for better accelarion and for realtime changes of ui elements
  */
 class GlassMainActivity : MainActivity()
