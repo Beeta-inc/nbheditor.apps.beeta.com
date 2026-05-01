3655a82f1856161ae7c5379e92f79e55d0e75a76
Beeta-inc <beeta.main@gmail.com>
2026-05-01 14:21:45 +0530

    imports update

--- appdata/Android/nbheditor/app/build.gradle.kts
+++ appdata/Android/nbheditor/app/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
     // Add the Google services Gradle plugin
     id("com.google.gms.google-services")
-    kotlin("android")
 }
 
 android {
